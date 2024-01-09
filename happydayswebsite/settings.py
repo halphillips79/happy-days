d7d40d6012a89bda6e853f0aa6274b0d02fa1407
halphillips <halphillips@gmail.com>
2024-01-09 15:30:02 -0500

    updated settings.py: allowed hosts

--- happydayswebsite/settings.py
+++ happydayswebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "happy-days-character-database.onrender.com").split(" ")
 
 # Application definition
 
